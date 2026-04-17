f745020faaaad2cf03d3cf4fcdf34904862bfea2
Zeno Maccari <zeno.maccari@studio.unibo.it>
2026-04-17 10:35:00 +0200

    Trim index.js

--- backend/index.js
+++ backend/index.js
@@ -1,80 +1,24 @@
-﻿/*
-File: index.js
-Author: Fabio Vitali
-Version: 1.0 
-Last change on: 2 April 2024
-
-
-Copyright (c) 2024 by Fabio Vitali
-
-   Permission to use, copy, modify, and/or distribute this software for any
-   purpose with or without fee is hereby granted, provided that the above
-   copyright notice and this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
-   SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
-   OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
-   CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
-*/
-
-
-
-/* ========================== */
-/*                            */
-/*           SETUP            */
-/*                            */
-/* ========================== */
-
-global.rootDir = __dirname ;
+﻿global.rootDir = __dirname ;
 global.startDate = null; 
 
-
-
-const template = require(global.rootDir + '/scripts/tpl.js');
-const mymongo = require(global.rootDir + '/scripts/mongo.js');
-
-require('dotenv').config();
+const mongoCredentials = {
+	user: process.env.DB_USER || "site242555",
+	pwd: process.env.DB_PASS || "Kahti2ho",
+	site: process.env.DB_HOST || "mongo_site242555"
+}  
 const express = require('express');
 const cors = require('cors')
 
 
-
-
-
-
-/* ========================== */
-/*                            */
-/*  EXPRESS CONFIG & ROUTES   */
-/*                            */
-/* ========================== */
-
 let app= express(); 
 
-app.use('/js'  , express.static(global.rootDir +'/public/js'));
-app.use('/css' , express.static(global.rootDir +'/public/css'));
-app.use('/data', express.static(global.rootDir +'/public/data'));
-app.use('/docs', express.static(global.rootDir +'/public/html'));
-app.use('/img' , express.static(global.rootDir +'/public/media'));
 app.use(express.urlencoded({ extended: true })) 
 app.use(cors())
 
 // https://stackoverflow.com/questions/40459511/in-express-js-req-protocol-is-not-picking-up-https-for-my-secure-link-it-alwa
 app.enable('trust proxy');
 
-
-app.get('/', async function (req, res) { 
-	let sitename = req.hostname.split('.')[0]
-	res.send(await template.generate('index.html', {
-			host: req.hostname,
-			site: sitename
-	}));
-})
-
-app.get('/hw', async function(req, res) { 
+app.get('/', async function(req, res) { 
 	var text = "Hello world as a Node service";
 	res.send(
 `<!doctype html>
@@ -87,68 +31,6 @@ app.get('/hw', async function(req, res) {
 			`)
 });
 
-app.get('/hwhb', async function(req, res) { 
-	res.send(await template.generate('generic.html', {
-		text: "Hello world as a Handlebar service",
-	}));
-});
-
-const info = async function(req, res) {
-	let data = {
-		startDate: global.startDate.toLocaleString(), 
-		requestDate: (new Date()).toLocaleString(), 
-		request: {
-			host: req.hostname,
-			method: req.method,
-			path: req.path,
-			protocol: req.protocol
-		}, 
-		query: req.query,
-		body: req.body
-	}
-	res.send( await template.generate('info.html', data));
-}
-
-app.get('/info', info )
-app.post('/info', info )
-
-
-
-
-
-/* ========================== */
-/*                            */
-/*           MONGODB          */
-/*       using mongoose       */
-/* ========================== */
-
-/* Replace these info with the ones you were given when activating mongoDB */ 
-const mongoCredentials = {
-	user: process.env.DB_USER || "site242555",
-	pwd: process.env.DB_PASS || "Kahti2ho",
-	site: process.env.DB_HOST || "mongo_site242555"
-}  
-
-/* end */
-
-app.get('/db/create', async function (req, res) {
-	res.send(await mymongo.create(mongoCredentials))
-});
-app.get('/db/search', async function (req, res) {
-	res.send(await mymongo.search(req.query, mongoCredentials))
-});
-
-
-
-
-
-
-
-/* ========================== */
-/*                            */
-/*    ACTIVATE NODE SERVER    */
-/*                            */
-/* ========================== */
 
 const PORT = process.env.PORT || 8000;
 
@@ -157,5 +39,3 @@ app.listen(PORT, function() {
 	console.log(`App listening on port ${PORT} started ${global.startDate.toLocaleString()}` )
 })
 
-
-/*       END OF SCRIPT        */
