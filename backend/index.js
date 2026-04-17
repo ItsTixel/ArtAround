c2c8c9f2520c1c0090351ed49880d9403aca62ea
Zeno Maccari <zeno.maccari@studio.unibo.it>
2026-04-17 11:00:22 +0200

    Minor change to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,43 @@
-﻿global.rootDir = __dirname ;
-global.startDate = null; 
+﻿global.rootDir = __dirname;
+global.startDate = null;
 
-const mongoCredentials = {
+const express = require('express');
+const cors = require('cors')
+const mongoose = require("mongoose");
+const credentials = {
 	user: process.env.DB_USER || "site242555",
 	pwd: process.env.DB_PASS || "Kahti2ho",
 	site: process.env.DB_HOST || "mongo_site242555"
-}  
-const express = require('express');
-const cors = require('cors')
+}
 
+let app = express();
 
-let app= express(); 
-
-app.use(express.urlencoded({ extended: true })) 
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+
 
 // https://stackoverflow.com/questions/40459511/in-express-js-req-protocol-is-not-picking-up-https-for-my-secure-link-it-alwa
 app.enable('trust proxy');
 
-app.get('/', async function(req, res) { 
-	var text = "Hello world as a Node service";
+(async () => {
+  try {
+	dbname = "artaround"
+    const mongouri = `mongodb://${credentials.user}:${credentials.pwd}@${credentials.site}/${dbname}?authSource=admin&writeConcern=majority`;
+    await mongoose.connect(mongouri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB", mongouri);
+	
+  } catch (e) {
+    console.error("Connection failed:", e.message);
+  }
+})();
+
+app.get('/', async function (req, res) {
+	var text = "Hello Duluze";
 	res.send(
-`<!doctype html>
+		`<!doctype html>
 <html>
 	<body>
 		<h1>${text}</h1>
@@ -34,8 +50,9 @@ app.get('/', async function(req, res) {
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, function() { 
-	global.startDate = new Date() ; 
-	console.log(`App listening on port ${PORT} started ${global.startDate.toLocaleString()}` )
+app.listen(PORT, function () {
+	global.startDate = new Date();
+	console.log(`App listening on port ${PORT} started ${global.startDate.toLocaleString()}`)
 })
+
 
