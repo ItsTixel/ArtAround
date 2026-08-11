ea100832d674d6d740c620fb06f1e1a720e5c329
Zeno Maccari <zeno.maccari@studio.unibo.it>
2026-08-11 20:28:33 +0200

    Minor UI fix in Navigator
    Su cellulare la pagina in Opera era considerata troppo lunga. Facendoti scorrere nul vuoto

--- navigator/src/layout/AppLayout.jsx
+++ navigator/src/layout/AppLayout.jsx
@@ -3,7 +3,7 @@ import BottomNav from '../components/BottomNav'
 
 function AppLayout() {
   return (
-    <div className="mx-auto flex min-h-screen max-w-md flex-col bg-bg">
+    <div className="mx-auto flex min-h-dvh max-w-md flex-col bg-bg">
       <main className="flex-1 overflow-y-auto pb-20">
         <Outlet />
       </main>
